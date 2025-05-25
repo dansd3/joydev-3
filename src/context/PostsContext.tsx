800e09cac0d52913cf9a8e5458b8af5dabd64a48
dansd3 <115929379+dansd3@users.noreply.github.com>
2025-05-25 23:18:38 +0300

    feat(filter): implemented posts filter
    Implemented posts filter, created "Select", "Input", "Checkbox" components, useDebounce hook.

--- src/context/PostsContext.tsx
+++ src/context/PostsContext.tsx
@@ -9,13 +9,14 @@ export interface PostData {
   authorName: string;
 }
 
-const PostsContext = createContext({ posts: [] as PostData[] });
+const PostsContext = createContext({ posts: [] as PostData[], loading: true });
 
 export const usePosts = () => useContext(PostsContext);
 
 export const PostsProvider = ({ children }: { children: React.ReactNode }) => {
   const [posts, setPosts] = useState<PostData[]>([]);
   const [initialized, setInitialized] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (initialized) return;
@@ -34,11 +35,12 @@ export const PostsProvider = ({ children }: { children: React.ReactNode }) => {
         }));
 
         setPosts(posts);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Ошибка загрузки постов:', error);
       });
   }, [initialized]);
 
-  return <PostsContext.Provider value={{ posts }}>{children}</PostsContext.Provider>;
+  return <PostsContext.Provider value={{ posts, loading }}>{children}</PostsContext.Provider>;
 };
